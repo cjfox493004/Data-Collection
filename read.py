da0e5e9b838b5df64cdecec92e75f6ad075cfb2c
cjfox493004 <cjfox493@gmail.com>
2026-03-02 15:44:36 +0000

    fun with queries

---
+++ read.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sqlmodel import Session, select
+from models import engine, Bio, Stats
+
+with Session(engine) as session:
+    statement = (
+        select(Bio.first_name, Bio.last_name, Bio.position)
+        .order_by(Bio.position, Bio.last_name)
+    )
+    
+    records = session.exec(statement).all()
+
+records_df = pd.DataFrame(records)
+print(records_df)
